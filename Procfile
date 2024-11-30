724460482a2359f60a269d2b364d34c761420522
tawhid2001 <tawhid.talal@gmail.com>
2024-11-30 07:08:04 -0500

    added

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: waitress-serve --port=8000 incom_expense_website.wsgi:application
+web: waitress-serve incom_expense_website.wsgi
--- incom_expense_website/settings.py
+++ incom_expense_website/settings.py
@@ -18,8 +18,7 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
